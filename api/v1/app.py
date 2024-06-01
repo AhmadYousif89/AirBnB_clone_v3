1233725c23c77f8eb8b2a4a48a38980e63cdab1d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-02 00:19:13 +0300

    Refactor CORS configuration in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ Flask Application """
+from os import environ
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask, make_response, jsonify
 from flasgger import Swagger
-from flasgger.utils import swag_from
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -40,10 +39,6 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)

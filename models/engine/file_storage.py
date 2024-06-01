296afd7a9cc2874b9bd796ffbe2cfbc954b965fd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-01 03:40:01 +0300

    Refactor save method in FileStorage class to use dictionary comprehension for JSON serialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,11 +46,11 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {}
-        for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
+            json.dump(
+                {key: self.__objects[key].to_dict() for key in self.__objects},
+                f,
+            )
 
     def reload(self):
         """deserializes the JSON file to __objects"""
